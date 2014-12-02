1787a9ce92b9a2b7d48c6bbb59a13676c61e7836
Christopher Williamson <chriswill0629@gmail.com>
2014-12-01 22:50:17 -0500

    Updated navbar to work with AngularJS.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -2,34 +2,34 @@
 
 /* App Module */
 
-var cawSite = angular.module('cawSite',[
+var cawSite = angular.module('cawSite', [
     'ngRoute',
     'cawControllers'
 ]);
 
 cawSite.config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider){
+    function ($routeProvider, $locationProvider) {
         $routeProvider.
                 when('/', {
                     templateUrl: 'partials/main.html',
                     controller: 'mainCtrl'
-        });
+                });
         $routeProvider.
                 when('/projects', {
                     templateUrl: 'partials/projects.html',
                     controller: 'projCtrl'
-        });
+                });
         $routeProvider.
                 when('/photography', {
                     templateUrl: 'partials/photography.html',
                     controller: 'photoCtrl'
-        });
+                });
         $routeProvider.
                 when('/design', {
                     templateUrl: 'partials/design.html',
                     controller: 'designCtrl'
-        });
-        
+                });
+
         $locationProvider.html5Mode(false).hashPrefix('!');
-        
+
     }]);
